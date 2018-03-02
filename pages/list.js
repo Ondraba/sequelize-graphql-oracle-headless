ee78ce19a45a9576d2082285d180c55359104828
oba <basista@fg.cz>
2018-03-02 09:03:19 +0100

    url routing

--- pages/list.js
+++ pages/list.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FullPageTemplate from "../client/edee_demo/layout/FullPageTemplate";
 import withData from "../lib/withData";
-import fetchEdeeStageDetail from "../../queries/fetchEdeeStageDetail";
+// import fetchEdeeStageDetail from "../../queries/fetchEdeeStageDetail";
 
 const List = props => (
   <FullPageTemplate title="List Page">
@@ -9,4 +9,4 @@ const List = props => (
   </FullPageTemplate>
 );
 
-export default graphql(fetchEdeeStageDetail)(List);
+export default List;
